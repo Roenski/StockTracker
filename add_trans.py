9a341055df151381771734a9ae9cb79ad14b2ec5
Roenski <aleksi.korsman@aalto.fi>
2021-02-19 22:41:40 +0100

    A message box, when an invalid entry has been added

--- add_trans.py
+++ add_trans.py
@@ -32,18 +32,22 @@ class Transaction(QtCore.QObject):
         self.tbroker = tbroker
     
     def verify(self):
-        self.tdate = self.verify_tdate(self.tdate)
-        self.ttype = self.verify_ttype(self.ttype)
-        self.tbs = self.verify_tbs(self.tbs)
-        self.sname = self.verify_sname(self.sname)
-        self.squantity = self.verify_squantity(self.squantity)
-        self.sprice = self.verify_sprice(self.sprice)
-        self.spricecurrency = self.verify_spricecurrency(self.spricecurrency)
-        self.spriceexchange = self.verify_spriceexchange(self.spriceexchange)
-        self.sfee = self.verify_sfee(self.sfee)
-        self.sfeecurrency = self.verify_sfeecurrency(self.sfeecurrency)
-        self.sfeeexchange = self.verify_sfeeexchange(self.sfeeexchange)
-        self.tbroker = self.verify_tbroker(self.tbroker)
+        try:
+            self.tdate = self.verify_tdate(self.tdate)
+            self.ttype = self.verify_ttype(self.ttype)
+            self.tbs = self.verify_tbs(self.tbs)
+            self.sname = self.verify_sname(self.sname)
+            self.squantity = self.verify_squantity(self.squantity)
+            self.sprice = self.verify_sprice(self.sprice)
+            self.spricecurrency = self.verify_spricecurrency(self.spricecurrency)
+            self.spriceexchange = self.verify_spriceexchange(self.spriceexchange)
+            self.sfee = self.verify_sfee(self.sfee)
+            self.sfeecurrency = self.verify_sfeecurrency(self.sfeecurrency)
+            self.sfeeexchange = self.verify_sfeeexchange(self.sfeeexchange)
+            self.tbroker = self.verify_tbroker(self.tbroker)
+            return True
+        except ValueError:
+            return False
         
     def verify_tdate(self, tdate):
         date = tdate.date()
@@ -52,73 +56,73 @@ class Transaction(QtCore.QObject):
             (date.year() == today.year and date.month() > today.month) or \
             (date.year() == today.year and date.month() == today.month and date.day() > today.day):
             self.erronous.emit("Date cannot be in the future!")
-            return 0
+            raise ValueError
         else:
             return "TO_DATE({}/{}/{})".format(date.year(), date.month(), date.day())
     def verify_ttype(self, ttype):
         if not ttype in ttypes:
-            print("Wrong type of commodity")
-            return 0
+            self.erronous.emit("Wrong type of commodity")
+            raise ValueError
         else:
             return ttype
     def verify_tbs(self, tbs):
         if not tbs in tBSs:
-            print("Wrong type of transaction")
-            return 0
+            self.erronous.emit("Wrong type of transaction")
+            raise ValueError
         else:
             return tbs
     def verify_sname(self, sname):
         if not sname in sNames:
-            print("Stock not added to the list")
-            return 0
+            self.erronous.emit("Stock not added to the list")
+            raise ValueError
         else:
             return sname
     def verify_squantity(self, squantity):
         if float(squantity) <= 0:
-            print("Stock quantity cannot be under 0. Use sell-type for sales.")
-            return 0
+            self.erronous.emit("Stock quantity cannot be under 0. Use sell-type for sales.")
+            raise ValueError
         else:
             return squantity
     def verify_sprice(self, sprice):
         if float(sprice) <= 0:
-            print("Stock price cannot be equal or less than 0")
-            return 0
+            self.erronous.emit("Stock price cannot be equal or less than 0")
+            raise ValueError
         else:
             return sprice
     def verify_spricecurrency(self, spricecurrency):
         if not spricecurrency in currencies:
-            print("Price currency not added to the list")
-            return 0
+            self.erronous.emit("Price currency not added to the list")
+            raise ValueError
         else:
             return spricecurrency
     def verify_spriceexchange(self, spriceexchange):
         if float(spriceexchange) <= 0:
-            print("Exchange rate cannot be negative or zero")
-            return 0
+            self.erronous.emit("Exchange rate cannot be negative or zero")
+            raise ValueError
         else:
             return spriceexchange
     def verify_sfee(self, sfee):
         if float(sfee) < 0:
-            print("Fee cannot be negative")
-            return 0
+            self.erronous.emit("Fee cannot be negative")
+            raise ValueError
         else:
             return sfee 
     def verify_sfeecurrency(self, sfeecurrency):
         if not sfeecurrency in currencies:
-            print("Fee currency not added to the list")
-            return 0
+            self.erronous.emit("Fee currency not added to the list")
+            raise ValueError
         else:
             return sfeecurrency
     def verify_sfeeexchange(self, sfeeexchange):
         if float(sfeeexchange) <= 0:
-            print("Exchange rate cannot be negative or zero.")
-            return 0
+            self.erronous.emit("Exchange rate cannot be negative or zero.")
+            raise ValueError
         else:
             return sfeeexchange
     def verify_tbroker(self, tbroker):
         if not tbroker in tBrokers:
-            print("Broker not added to the list")
-            return 0
+            self.erronous.emit("Broker not added to the list")
+            raise ValueError
         else:
             return tbroker
 
@@ -154,9 +158,16 @@ class AddTransactionForm(QtWidgets.QWidget):
             self.ui.combo_tBroker.currentText()
         )
         trans.erronous.connect(self.errorprinter)
-        trans.verify()
+        if not trans.verify():
+            self.errorprinter("Did you leave some fields blank? Don't.")
 
-    def errorprinter(self, msg):
-        print(msg)
+    def errorprinter(self, er_msg):
+        #print(msg)
+        msg = QtWidgets.QMessageBox()
+        msg.setIcon(QtWidgets.QMessageBox.Warning)
+        msg.setText("Error adding value")
+        msg.setInformativeText(er_msg)
+        msg.setStandardButtons(QtWidgets.QMessageBox.Ok)
+        msg.exec_()
 
 
